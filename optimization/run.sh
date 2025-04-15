9f095912cea170915d57d5cf461bb0d73aae15bf
David Woller <wolledav@fel.cvut.cz>
2025-04-15 17:13:53 +0200

    MCSP eval. script added

--- optimization/run.sh
+++ optimization/run.sh
@@ -1,63 +1,35 @@
 #!/bin/bash
 
-# INSTANCE_DIR="./data/instances_v4/naive_21/"
+INSTANCE_DIR="./data/instances_v4/weekly_21/"
 
-# # Generate unit solutions for proportional instances
-# OUTPUT="./results/v4_unit/naive_21/"
-# SCRIPT="./optimization/models/rebalance_unit.py"
-# INSTANCES="proportional"
-# TIMEOUT=3600
+# Generate unit solutions for proportional instances
+OUTPUT="./results/v4_unit/weekly_21/"
+SCRIPT="./optimization/models/rebalance_unit.py"
+INSTANCES="proportional"
+TIMEOUT=3600
 
-# for INSTANCE in "$INSTANCE_DIR"*.json; do
-#     if [[ "$INSTANCE" == *$INSTANCES* ]]; then
-#         echo "Solving instance: $INSTANCE"
-#         python3 $SCRIPT -i $INSTANCE -o $OUTPUT -t $TIMEOUT -f v4 -r
-#     fi
-# done
+for INSTANCE in "$INSTANCE_DIR"*.json; do
+    if [[ "$INSTANCE" == *$INSTANCES* ]]; then
+        echo "Solving instance: $INSTANCE"
+        python3 $SCRIPT -i $INSTANCE -o $OUTPUT -t $TIMEOUT -f v4 -r
+    fi
+done
 
-# # Generate cbws solutions for proportional instances 
-# OUTPUT="./results/v4_cbws/naive_21/"
-# SCRIPT="./optimization/models/rebalance_v4_cbws.py"
-# INSTANCES="proportional"
-# TIMEOUT=3600
+# Generate cbws solutions for proportional instances 
+OUTPUT="./results/v4_cbws/weekly_21/"
+SCRIPT="./optimization/models/rebalance_v4_cbws.py"
+INSTANCES="proportional"
+TIMEOUT=3600
 
-# for INSTANCE in "$INSTANCE_DIR"*.json; do
-#     if [[ "$INSTANCE" == *$INSTANCES* ]]; then
-#         echo "Solving instance: $INSTANCE"
-#         python3 $SCRIPT -i $INSTANCE -o $OUTPUT -t $TIMEOUT -r
-#     fi
-# done
-
-# # Generate cb solutions for all instances
-# OUTPUT="./results/v4_cb/naive_21/"
-# SCRIPT="./optimization/models/rebalance_unit.py"
-# TIMEOUT=3600
-
-# for INSTANCE in "$INSTANCE_DIR"*.json; do
-#     echo "Solving instance: $INSTANCE"
-#     python3 $SCRIPT -i $INSTANCE -o $OUTPUT -t $TIMEOUT -f v4_cb -r
-# done
-
-# Generate v5 solutions for all instances
-# OUTPUT="./results/v5/naive_21/"
-# SCRIPT="./optimization/models/rebalance_v5.py"
-# TIMEOUT=3600
-
-# for INSTANCE in "$INSTANCE_DIR"*.json; do
-#     echo "Solving instance: $INSTANCE"
-#     python3 $SCRIPT -i $INSTANCE -o $OUTPUT -t $TIMEOUT
-# done
-
-
-
-
-
-
-
-INSTANCE_DIR="./data/instances_v4/rnd_large_3/"
+for INSTANCE in "$INSTANCE_DIR"*.json; do
+    if [[ "$INSTANCE" == *$INSTANCES* ]]; then
+        echo "Solving instance: $INSTANCE"
+        python3 $SCRIPT -i $INSTANCE -o $OUTPUT -t $TIMEOUT -r
+    fi
+done
 
 # Generate cb solutions for all instances
-OUTPUT="./results/v4_cb/rnd_large_3/"
+OUTPUT="./results/v4_cb/naive_21/"
 SCRIPT="./optimization/models/rebalance_unit.py"
 TIMEOUT=3600
 
@@ -67,7 +39,7 @@ for INSTANCE in "$INSTANCE_DIR"*.json; do
 done
 
 # Generate v5 solutions for all instances
-OUTPUT="./results/v5/rnd_large_3/"
+OUTPUT="./results/v5/naive_21/"
 SCRIPT="./optimization/models/rebalance_v5.py"
 TIMEOUT=3600
 
@@ -75,3 +47,31 @@ for INSTANCE in "$INSTANCE_DIR"*.json; do
     echo "Solving instance: $INSTANCE"
     python3 $SCRIPT -i $INSTANCE -o $OUTPUT -t $TIMEOUT
 done
+
+
+
+
+
+
+
+# INSTANCE_DIR="./data/instances_v4/rnd_large_3/"
+
+# # Generate cb solutions for all instances
+# OUTPUT="./results/v4_cb/rnd_large_3/"
+# SCRIPT="./optimization/models/rebalance_unit.py"
+# TIMEOUT=3600
+
+# for INSTANCE in "$INSTANCE_DIR"*.json; do
+#     echo "Solving instance: $INSTANCE"
+#     python3 $SCRIPT -i $INSTANCE -o $OUTPUT -t $TIMEOUT -f v4_cb -r
+# done
+
+# # Generate v5 solutions for all instances
+# OUTPUT="./results/v5/rnd_large_3/"
+# SCRIPT="./optimization/models/rebalance_v5.py"
+# TIMEOUT=3600
+
+# for INSTANCE in "$INSTANCE_DIR"*.json; do
+#     echo "Solving instance: $INSTANCE"
+#     python3 $SCRIPT -i $INSTANCE -o $OUTPUT -t $TIMEOUT
+# done
--- optimization/utils/eval_customer_satisfaction.py
+++ optimization/utils/eval_customer_satisfaction.py
@@ -3,21 +3,67 @@
 import json
 import os
 import numpy as np
+import re
 
-instance = "data/instances_v4/naive_2024-12-28_duration/NTU.json"
-demand_timestep = 300 # seconds
-period = 24 * 60 * 60 # seconds
-steps = period // demand_timestep
-print(steps)
+from inventory_policies import *
 
-data = {}
-with open(instance, 'r') as f:
-    data = json.load(f)
-    for st in data['stations']:
-        capacity = st["capacity"]
-        random_demands = np.random.randint(-capacity, capacity + 1, size=steps).tolist()
-        st["demands"] = random_demands
+is_mapping = {"proportional": "prop"
+        }
 
-    data["constants"]["demand_timestep"] = demand_timestep
-    
-print(data)
+
+# instance = "data/instances_v4/weekly_21/weekly_2025-03-24_proportional.json"
+
+instances_dir = "data/instances_v4/weekly_21/"
+policy = "proportional"
+
+instances = os.listdir(instances_dir)
+
+table_data = []
+
+print("name\t\t\ttotal_demand\treal_P_mean\treal_Q_total")
+
+for instance in sorted(instances):
+    if policy in instance and "unit" not in instance:
+        data = {}
+        with open(instances_dir + instance, 'r') as f:
+            data = json.load(f)
+            
+        real_P_max_vals = []
+        real_Q_total_vals = []
+        real_demand_sums = []
+
+        for station in data['stations']:
+            capacity = station['capacity']
+            s_goal = station['s_goal']
+            ideal_s_goal = station['real_s_goal']
+
+            real_demand = station['real_demand']
+
+            real_inventories = get_inventories(real_demand, s_goal, capacity)
+
+            real_P_max_i = get_P_max(real_demand, real_inventories, capacity)
+
+            real_Q_total_i = get_Q_total(real_demand, real_inventories, capacity)
+
+            real_demand_abs = [abs(d) for d in real_demand]
+            real_demand_sum = sum(real_demand_abs)
+
+            real_P_max_vals.append(real_P_max_i)
+            real_Q_total_vals.append(real_Q_total_i)
+            real_demand_sums.append(real_demand_sum)
+
+        real_P_mean = np.mean(real_P_max_vals)
+        real_demand_sum = sum(real_demand_sums)
+        real_Q_total = 100 * (1 - sum(real_Q_total_vals) / real_demand_sum)
+
+
+        match = re.search(r'(\d{4}-\d{2}-\d{2})[^_]*_(\w+)\.json$', instance)
+        name = instance.replace(".json", "")
+        if match:
+            date = match.group(1)
+            last_word = is_mapping[match.group(2)]
+            name = f"{date}_{last_word}"
+
+
+        # table_data.append([name, demand_sum, real_P_mean, Q_rel_percent])
+        print(f"{name}\t\t{real_demand_sum}\t\t{real_P_mean:.2f}\t\t{real_Q_total:.2f}")
